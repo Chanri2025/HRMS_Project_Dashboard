ea43af87796f59f4bf4ba96ff2566cca13f5e875
Chanri2025 <aniruddhag.appynitty@gmail.com>
2025-11-10 17:26:43 +0530

    Development - XXI

--- src/Component/auth/LandingPage.jsx
+++ src/Component/auth/LandingPage.jsx
@@ -6,7 +6,7 @@ const LandingPage = ({ setIsAuthenticated }) => {
   return (
     <div className="flex h-screen w-screen bg-white overflow-hidden flex-col justify-center items-center">
       <div className="bg-white p-10 rounded-lg shadow-lg border w-[450px] text-center">
-        <h1 className="text-2xl font-bold mb-4">Welcome to Appynitty Portal</h1>
+        <h1 className="text-2xl font-bold mb-4">Welcome to Appynitty HRMS Portal</h1>
 
         <img src={Logo} alt="SM Logo" className="mb-4 w-32 h-auto mx-auto" />
 
